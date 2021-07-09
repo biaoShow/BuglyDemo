4733920914dc1dab57110ddf88d30781d372ef5a
liyuanbiao <liyuanbiao@gitlab.dm-ai.cn>
2021-07-09 10:26:52 +0800

    1

---
+++ app/src/main/java/com/biao/buglydemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.biao.buglydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        BugClass.bug();
+    }
+}
